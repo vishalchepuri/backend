9f75c4e96bf416490460edd3b02bff2bfec1b96d
Vishal <67873373+vishalchepuri@users.noreply.github.com>
2024-04-10 23:33:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,55 @@
-const path = require('path');
+// const path = require('path');
 
+// const express = require('express');
+// const bodyParser = require('body-parser');
+
+// const errorController = require('./controllers/error');
+
+// const app = express();
+
+// app.set('view engine', 'ejs');
+// app.set('views', 'views');
+
+// const adminRoutes = require('./routes/admin');
+// const shopRoutes = require('./routes/shop');
+
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/admin', adminRoutes);
+// app.use(shopRoutes);
+
+// app.use(errorController.get404);
+
+// app.listen(8080);
+
+
+
+
+
+
+
+
+// ------new code
+
+
+
+const path = require('path');
+const fs = require('fs');
+const https = require('https');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const hostname = 'wezeal.me';
+const httpsPort = 443;
 const errorController = require('./controllers/error');
-
 const app = express();
+
+const httpsOptions = {
+    cert: fs.readFileSync('./ssl/wezeal_me.crt'),
+    ca: fs.readFileSync('./ssl/wezeal_me.ca-bundle'), 
+    key: fs. readFileSync('./ssl/wezeal_me.key')
+}
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -21,4 +65,5 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(8080);
+const httpsServer = https.crogteServer(httpsOptions, app);
+httpsServer.listen(httpsPort, hostname)
