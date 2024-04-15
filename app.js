1dcb3a396350b1f72bc2ae63a6e0a571e36544e4
Vishal <67873373+vishalchepuri@users.noreply.github.com>
2024-04-15 13:30:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,41 +1,5 @@
-// const path = require('path');
-
-// const express = require('express');
-// const bodyParser = require('body-parser');
-
-// const errorController = require('./controllers/error');
-
-// const app = express();
-
-// app.set('view engine', 'ejs');
-// app.set('views', 'views');
-
-// const adminRoutes = require('./routes/admin');
-// const shopRoutes = require('./routes/shop');
-
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use('/admin', adminRoutes);
-// app.use(shopRoutes);
-
-// app.use(errorController.get404);
-
-// app.listen(8080);
-
-
-
-
-
-
-
-
-// ------new code
-
-
 const path = require('path');
-const fs = require('fs');
-const https = require('https');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -57,11 +21,47 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-// SSL certificates
-const options = {
-  cert: fs.readFileSync(path.join(__dirname, './ssl/wezeal_me.crt')),
-  ca: fs.readFileSync(path.join(__dirname, './ssl/wezeal_me.ca-bundle')),
-  key: fs.readFileSync(path.join(__dirname, './ssl/wezeal_me.key')),
-};
+app.listen(8080);
 
-https.createServer(options, app).listen(443);
+
+
+
+
+
+
+
+// ------new code
+
+
+// const path = require('path');
+// const fs = require('fs');
+// const https = require('https');
+// const express = require('express');
+// const bodyParser = require('body-parser');
+
+// const errorController = require('./controllers/error');
+
+// const app = express();
+
+// app.set('view engine', 'ejs');
+// app.set('views', 'views');
+
+// const adminRoutes = require('./routes/admin');
+// const shopRoutes = require('./routes/shop');
+
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/admin', adminRoutes);
+// app.use(shopRoutes);
+
+// app.use(errorController.get404);
+
+// // SSL certificates
+// const options = {
+//   cert: fs.readFileSync(path.join(__dirname, './ssl/wezeal_me.crt')),
+//   ca: fs.readFileSync(path.join(__dirname, './ssl/wezeal_me.ca-bundle')),
+//   key: fs.readFileSync(path.join(__dirname, './ssl/wezeal_me.key')),
+// };
+
+// https.createServer(options, app).listen(443);
